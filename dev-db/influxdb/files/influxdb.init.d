3d345970608d6e7d7610ed96fada43ce86661bd7
Josh Bowers <josh@cloudopsgroup.com>
2021-02-06 14:07:12 -0800

    fix some weirdness

--- dev-db/influxdb/files/influxdb.init.d
+++ dev-db/influxdb/files/influxdb.init.d
@@ -3,7 +3,6 @@
 # Distributed under the terms of the GNU General Public License v2
 # $Id$
 
-
 depend() {
   need net
   after bootmisc
@@ -12,14 +11,14 @@ depend() {
 start() {
     ebegin "Starting influxdb server"
 
-  export INFLUXD_CONFIG_PATH=/etc/influxdb/influxdb.yaml
+    export INFLUXD_CONFIG_PATH=/etc/influxdb/influxdb.yaml
 
-    start-stop-daemon --start -b \ 
-        --user influxdb:influxdb \ 
-  --pidfile /run/influxdb.pid \ 
-  --make-pidfile \ 
-  --stdout /var/log/influxdb/influxd.log \ 
-  --stderr /var/log/influxdb/influxd.log \ 
+    start-stop-daemon --start -b \
+        --user influxdb:influxdb \
+  --pidfile /run/influxdb.pid \
+  --make-pidfile \
+  --stdout /var/log/influxdb/influxd.log \
+  --stderr /var/log/influxdb/influxd.log \
   --exec /usr/bin/influxd
     eend $?
 }
@@ -27,8 +26,8 @@ start() {
 stop() {
     ebegin "Stopping influxdb server"
 
-    start-stop-daemon --stop \ 
-  --pidfile /run/influxdb.pid \ 
+    start-stop-daemon --stop \
+  --pidfile /run/influxdb.pid \
   --exec /usr/bin/influxd
     eend $?
 }
